d0e333642e3510851b6980ef6266e0c1577d3bcb
Madzhun Khakim <hakimmadgun@gmail.com>
2023-03-27 20:09:49 +0600

    created project connect it to database postgresql

---
+++ src/main/java/com/example/LabWorkMarch_2/LabWorkMarch2Application.java
@@ -0,0 +1,13 @@
+package com.example.LabWorkMarch_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabWorkMarch2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabWorkMarch2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LabWorkMarch_2/LabWorkMarch2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LabWorkMarch_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabWorkMarch2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
